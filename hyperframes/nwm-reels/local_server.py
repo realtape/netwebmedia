8acbce5c38a4171af6e30ce1cb7928a977322fa3
realtape <entrepoker@gmail.com>
2026-04-28 18:52:12 -0400

    backup: auto-save 2026-04-28 18:52

--- hyperframes/nwm-reels/local_server.py
+++ hyperframes/nwm-reels/local_server.py
@@ -1,16 +1,19 @@
-"""Serve renders/ with CORS headers on port 8765."""
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 
 class CORSHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Access-Control-Allow-Private-Network', 'true')
         super().end_headers()
+    def do_OPTIONS(self):
+        self.send_response(204)
+        self.end_headers()
     def log_message(self, fmt, *args):
         print(fmt % args, flush=True)
 
 os.chdir(r'C:\Users\Usuario\Desktop\NetWebMedia\hyperframes\nwm-reels\renders')
-print('Serving renders/ on http://localhost:8765', flush=True)
+print('Serving with Private-Network header on http://localhost:8765', flush=True)
 HTTPServer(('localhost', 8765), CORSHandler).serve_forever()

d9b6a8a53f4116ff634c3d4d986cb378d07ada57
realtape <entrepoker@gmail.com>
2026-04-28 18:29:51 -0400

    backup: auto-save 2026-04-28 18:29

--- crm-vanilla/api/index.php
+++ crm-vanilla/api/index.php
@@ -59,6 +59,7 @@ $handlers = [
     'import_csv'    => __DIR__ . '/handlers/import_csv.php',
     'import_best'   => __DIR__ . '/handlers/import_best.php',
     'realtime'      => __DIR__ . '/handlers/realtime.php',
+    'niche_config'  => __DIR__ . '/handlers/niche_config.php',
 ];
 
 if (!isset($handlers[$resource])) {
